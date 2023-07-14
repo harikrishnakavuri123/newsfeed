f50004c96477572da78357e129887d0ce4b70439
harikrishnakavuri123 kavuri <harikrishnakavuri123@gmail.com>
2023-07-14 17:17:23 +0000

    hari

---
+++ index.js
@@ -0,0 +1,22 @@
+var newsFeed = document.getElementById("news-feed");
+var buttons = document.getElementById("buttons");
+var loadSavedButton = document.getElementById("load-saved");
+var loadNewButton = document.getElementById("load-new");
+var likeButton = document.getElementById("like");
+var categorySelect = document.getElementById("category");
+
+loadSavedButton.onclick = function() {
+  // Load saved news articles.
+};
+
+loadNewButton.onclick = function() {
+  // Load new news articles.
+};
+
+likeButton.onclick = function() {
+  // Like the current news article.
+};
+
+categorySelect.onchange = function() {
+  // Change the category of news articles.
+};
